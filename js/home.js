8ad232de672afe8140dc644385a9c89fd5a0e347
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-09-19 03:39:14 -0700

    Verify Pin Number and add money

--- js/home.js
+++ js/home.js
@@ -1,3 +1,15 @@
 document
   .getElementById("add-btn")
-  .addEventListener("click", function handleAddBtn() {});
+  .addEventListener("click", function handleAddBtn(event) {
+    event.preventDefault();
+    const pinNumber = document.getElementById("pin").value;
+
+    if (pinNumber === "1234") {
+      const balance = parseInt(document.getElementById("balance").innerText);
+      const amount = parseInt(document.getElementById("amount").value);
+      const updatedBalance = balance + amount;
+      document.getElementById("balance").innerText = updatedBalance;
+    } else {
+      alert("Wrong pin");
+    }
+  });
